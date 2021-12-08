ab5983a4b83a8cd7c47a1992b4cd17f63793b62a
lucguillemot <luc.guillemot@gmail.com>
2021-12-08 17:28:00 +0100

    feat: add trial page

---
+++ src/routes/trial.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<script>
+  import Dots from "$components/force-layout/Dots.svelte";
+  import State from "$components/force-layout/State.svelte";
+</script>
+
+<h1>Gods Pantheon Cartography</h1>
+
+<div>
+  <State>
+    <Dots />
+  </State>
+</div>
