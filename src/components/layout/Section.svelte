9fab03c99a38e9ee0a8a60d42fba4e111cec4271
lucguillemot <luc.guillemot@gmail.com>
2022-01-12 10:12:42 +0100

    feat: add basic Section component

---
+++ src/components/layout/Section.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let fullBleed = false;
+  export let id = undefined;
+</script>
+
+<section {id} class:centered={!fullBleed}>
+  <slot />
+</section>
+
+<style>
+  section {
+    margin: 0 auto;
+  }
+
+  .centered {
+    padding: 0 1rem;
+    max-width: 90rem;
+  }
+</style>
