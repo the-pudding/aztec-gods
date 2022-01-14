65d37eb0e631b32888212863317417f33750010f
lucguillemot <luc.guillemot@gmail.com>
2022-01-14 13:26:23 +0100

    feat: add intro copy

---
+++ src/components/introduction/Intro.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import doc from "$data/doc.json";
+  import Heading from "$components/layout/Heading.svelte";
+</script>
+
+{#each doc.introduction as p}
+  <p>{p}</p>
+{/each}
+
+<Heading>{doc.sources_title}</Heading>
+{#each doc.sources as p}
+  <p>{p.text}</p>
+{/each}
+
+<Heading>{doc.iconography_title}</Heading>
+{#each doc.iconography as p}
+  <p>{p.text}</p>
+{/each}
