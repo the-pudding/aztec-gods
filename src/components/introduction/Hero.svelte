d731f3acd815b5b7c6fc5791ea86793cacadfb4d
lucguillemot <luc.guillemot@gmail.com>
2022-01-17 10:20:15 +0100

    feat: add placeholder Hero component

---
+++ src/components/introduction/Hero.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  import doc from "$data/doc.json";
+  import Section from "$components/layout/Section.svelte";
+</script>
+
+<Section>
+  <h1>
+    {doc.title}
+  </h1>
+</Section>
