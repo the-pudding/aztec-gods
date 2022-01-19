aed074e59077c26865c917c5fb8f4ff0df8f7b07
lucguillemot <luc.guillemot@gmail.com>
2022-01-19 21:20:29 +0100

    chore: clean up

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,22 +1,19 @@
 <script context="module">
   export const prerender = true;
-  import doc from "$data/doc.json";
+  import Controls from "$components/force-layout/Controls.svelte";
+  import Gods from "$components/force-layout/Gods.svelte";
+  import Links from "$components/force-layout/Links.svelte";
+  import Overlay from "$components/force-layout/Overlay.svelte";
+  import State from "$components/force-layout/State.svelte";
+  import Tezca from "$components/iconography/Tezca.svelte";
+  import Tlalte from "$components/iconography/Tlalte.svelte";
+  import Hero from "$components/introduction/Hero.svelte";
+  import Intro from "$components/introduction/Intro.svelte";
+  import Scrolly from "$components/map/Scrolly.svelte";
+  import Sources from "$components/Sources.svelte";
 </script>
 
 <script>
-  import Sources from "$components/Sources.svelte";
-  import Heading from "$components/layout/Heading.svelte";
-  import Intro from "$components/introduction/Intro.svelte";
-  import Hero from "$components/introduction/Hero.svelte";
-  import Links from "$components/force-layout/Links.svelte";
-  import State from "$components/force-layout/State.svelte";
-  import Gods from "$components/force-layout/Gods.svelte";
-  import Overlay from "$components/force-layout/Overlay.svelte";
-  import Controls from "$components/force-layout/Controls.svelte";
-  import Scrolly from "$components/map/Scrolly.svelte";
-  import Tezca from "$components/iconography/Tezca.svelte";
-  import Tlalte from "$components/iconography/Tlalte.svelte";
-  import Geometric from "$components/map/Geometric.svelte";
 </script>
 
 <Hero />
@@ -35,7 +32,7 @@
   </g>
   <Gods slot="chart-html" />
   <g slot="chart-svg-overlay">
-    <Overlay debug />
+    <Overlay />
   </g>
   <Controls slot="controls" />
 </State>
