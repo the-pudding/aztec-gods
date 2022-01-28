4238377fa81127ce82cada16b16080f9cb17bd03
lucguillemot <luc.guillemot@gmail.com>
2022-01-28 11:03:13 +0100

    chore: clean up

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,12 +1,11 @@
 <script context="module">
   export const prerender = true;
-  import Tezca from "$components/iconography/Tezca.svelte";
   import Iconography from "$components/iconography/Iconography.svelte";
-  import Tlalte from "$components/iconography/Tlalte.svelte";
   import Hero from "$components/introduction/Hero.svelte";
   import Intro from "$components/introduction/Intro.svelte";
   import Scrolly from "$components/map/Scrolly.svelte";
-  import Sources from "$components/Sources.svelte";
+  import Sources from "$components/colophon/Sources.svelte";
+
   import ContextualElements from "$components/ContextualElements.svelte";
 </script>
 
