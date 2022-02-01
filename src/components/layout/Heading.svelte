1903695d68b2ac7eb5e4d22660ee91abc01c8f7f
lucguillemot <luc.guillemot@gmail.com>
2022-02-01 09:43:22 +0100

    feat: make heading and paragraph responsive

--- src/components/layout/Heading.svelte
+++ src/components/layout/Heading.svelte
@@ -2,9 +2,23 @@
 
 <style>
   h2 {
-    margin: 4rem 0 1rem 0;
-    font-size: 3rem;
+    margin: 2rem 0 1rem 0;
+    font-size: 2rem;
     line-height: 1.2;
     text-transform: uppercase;
+  }
+
+  @media only screen and (min-width: 30em) {
+    h2 {
+      margin: 3rem 0 1rem 0;
+      font-size: 2.5rem;
+    }
+  }
+
+  @media only screen and (min-width: 50em) {
+    h2 {
+      margin: 4rem 0 1rem 0;
+      font-size: 3rem;
+    }
   }
 </style>
--- src/components/layout/Paragraph.svelte
+++ src/components/layout/Paragraph.svelte
@@ -4,9 +4,15 @@
   p {
     max-width: 42em;
     margin: 1rem 0;
-    line-height: 1.65;
-    text-align: left;
-    font-size: 1.125rem;
     font-family: var(--serif);
+    text-align: left;
+    font-size: 1rem;
+    line-height: 1.65;
+  }
+
+  @media only screen and (min-width: 30em) {
+    p {
+      font-size: 1.125rem;
+    }
   }
 </style>
