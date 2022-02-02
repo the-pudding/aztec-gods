b0fdad3ace96a33d8d9c5554f3cead16407edec0
Luc Guillemot <luc.guillemot@gmail.com>
2022-02-02 09:39:48 +0100

    Step update (#24)
    * chore: clean up
    
    * feat: add TextButton Component
    
    * feat: add god name in step
    
    * fix: add button hover behavior
    
    * feat: add additional infos to step
    
    * feat: add resources to step
    
    * fix: clean up

---
+++ src/components/layout/TextButton.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Icon from "$components/helpers/Icon.svelte";
+  export let disabled = false;
+  export let iconName = undefined;
+  export let position = "start"; // "start" | "end"
+  export let buttonLabel = "";
+  export let handleClick;
+</script>
+
+<button {disabled} on:click={handleClick} style="justify-content: {position}">
+  {#if iconName && buttonLabel && position === "start"}
+    <Icon name={iconName} />
+  {/if}
+  {#if buttonLabel}
+    {buttonLabel}
+  {/if}
+  {#if iconName && buttonLabel && position === "end"}
+    <Icon name={iconName} />
+  {/if}
+</button>
+
+<style>
+  button {
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    background: transparent;
+    border: none;
+
+    text-decoration: underline;
+    font-size: 0.825rem;
+
+    color: currentColor;
+
+    margin: 0;
+    padding: 0;
+  }
+  button:hover {
+    text-decoration-thickness: 2px;
+  }
+  button:disabled {
+    cursor: unset;
+  }
+</style>
