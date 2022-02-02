acccf417e03a9d7cb80f2050b04f4ee9269040ee
lucguillemot <luc.guillemot@gmail.com>
2022-02-02 09:42:44 +0100

    update github pages

--- docs/_app/pages/index.svelte-12f87e1e.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-2a373ff0.js";import{R as a,p as d}from"../chunks/index-7ea39448.js";import"../chunks/preload-helper-7bf1142d.js";export{a as default,d as prerender};
---
+++ docs/_app/pages/index.svelte-2050c107.js
@@ -0,0 +1,1 @@
+import"../chunks/vendor-d10c119e.js";import{R as p,p as a}from"../chunks/index-e6ae460a.js";import"../chunks/preload-helper-7bf1142d.js";export{p as default,a as prerender};
