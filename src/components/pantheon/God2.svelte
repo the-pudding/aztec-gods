1a7d79d386f0f8250af10ae0919b91cbed7e4425
lucguillemot <luc.guillemot@gmail.com>
2022-02-02 13:27:19 +0100

    feat: use secondary color for selected state

--- src/components/pantheon/God2.svelte
+++ src/components/pantheon/God2.svelte
@@ -52,17 +52,16 @@
   $: isFullWidth = storyMode && activeStep.id === god.name;
 
   // Interaction Parameters
-
+  // Highlight = hover
+  // Selection = selected
   $: highlightExists = $interaction !== undefined;
   $: isHighlighted = highlightExists && $interaction === god.name;
+  $: isHighlightRelated = highlightExists && highlightRelatedGods.includes(god.name);
 
   $: selectionExists = $selection !== undefined;
   $: isSelected = selectionExists && getName($selection) === god.name;
-
-  $: isRelated =
-    (selectionExists && selectionRelatedGods.includes(god.name)) ||
-    (highlightExists && highlightRelatedGods.includes(god.name));
-
+  $: isSelectionRelated = selectionExists && selectionRelatedGods.includes(god.name);
+  $: isRelated = isHighlightRelated || isSelectionRelated;
   const getOpacity = (
     storyMode,
     isMain,
@@ -72,7 +71,6 @@
     highlightExists,
     isHighlighted
   ) => {
-    // console.log(selection);
     if (storyMode && isMain) {
       return 1;
     } else if (!storyMode && !selectionExists && !highlightExists) {
@@ -99,7 +97,12 @@
   // Geometric parameters
   $: rad = $radiusScale(getImportance(god));
   $: borderWidth = !isMain ? 0 : rad * 0.07;
-  $: bgColor = !isMain ? variables.category.secondary : "white"; // getLightGodColor(god.importance);
+  $: color =
+    selectionExists && (isSelected || isSelectionRelated)
+      ? variables.color.secondary
+      : !isMain
+      ? variables.category.secondary
+      : getMainGodColor(god.importance);
 
   $: x = $xScale(god[$linkHighlight].x) + $bounds.margins.left;
   $: y = $yScale(god[$linkHighlight].y) + $bounds.margins.top;
@@ -113,13 +116,14 @@
   left:{isFullWidth ? $bounds.chartWidth / 2 + $bounds.margins.left : x}px; top:{isFullWidth
     ? $bounds.chartHeight / 2 + $bounds.margins.top
     : y}px; 
-  background-color: {bgColor};
-  transform: translate(-50%, -50%) {isHighlighted ? `scale(1.3)` : `scale(1)`};
+  background-color: {color};
+  transform: translate(-50%, -50%);
   z-index: {isFullWidth ? 200 : 20};
-  border: {borderWidth}px solid {getMainGodColor(god.importance)};
+  border: {borderWidth}px solid {color};
   opacity:{opacity};
   "
 />
+<!-- {isHighlighted ? `scale(1.3)` : `scale(1)`} -->
 
 <!-- background-image: {isMain ? `url(${img.src})` : 'unset'}; -->
 <style>
@@ -133,7 +137,7 @@
     align-items: center;
     background-size: cover;
     transition: opacity 500ms, transform 500ms, border-width 500ms, left 1000ms, top 1000ms,
-      width 1000ms, height 1000ms, filter 500ms;
+      width 1000ms, height 1000ms;
 
     pointer-events: none;
   }
--- src/components/pantheon/Links.svelte
+++ src/components/pantheon/Links.svelte
@@ -1,29 +1,37 @@
 <script>
+  import { fade } from "svelte/transition";
   import { getContext } from "svelte";
   import variables from "$data/variables.json";
   const { bounds, xScale, yScale, interaction, selection, getName, currentLinks } =
     getContext("chart-state");
 </script>
 
-<g data-name="links" transform={`translate(${$bounds.margins.left}, ${$bounds.margins.top})`}>
+<g
+  in:fade
+  data-name="links"
+  transform={`translate(${$bounds.margins.left}, ${$bounds.margins.top})`}
+>
   {#each $currentLinks as link}
     <line
       x1={$xScale(link.source.x)}
       y1={$yScale(link.source.y)}
       x2={$xScale(link.target.x)}
       y2={$yScale(link.target.y)}
-      stroke={variables.color.highlight}
+      stroke={$selection && getName($selection) === getName(link.source)
+        ? variables.color.secondary
+        : variables.color.highlight}
       stroke-opacity={($selection && getName($selection) === getName(link.source)) ||
       ($interaction && $interaction === getName(link.source))
-        ? 0.3
+        ? 1
         : 0}
-      stroke-width={$selection && getName($selection) === getName(link.source) ? 2 : 1}
+      stroke-width={$selection && getName($selection) === getName(link.source) ? 2 : 2}
     />
   {/each}
 </g>
 
 <style>
-  line {
-    transition: opacity 500ms;
+  line,
+  g {
+    transition: stroke-opacity 500ms, x1 500ms, x2 500ms, y1 500ms, y2 500ms;
   }
 </style>
