cfc0c5cdc8c18ff5b4517d095695c88edb2ec905
lucguillemot <luc.guillemot@gmail.com>
2022-02-17 15:28:34 +0100

    feat: constrain hover on selection related gods

--- src/components/pantheon/Overlay.svelte
+++ src/components/pantheon/Overlay.svelte
@@ -7,8 +7,17 @@
 
   let overlay; // ref
 
-  const { bounds, xScale, yScale, getName, nodes, interaction, selection, linkHighlight } =
-    getContext("chart-state");
+  const {
+    bounds,
+    xScale,
+    yScale,
+    getName,
+    nodes,
+    interaction,
+    selection,
+    currentLinks,
+    linkHighlight
+  } = getContext("chart-state");
 
   // Overlay Logic
   $: delaunay = Delaunay.from(
@@ -18,34 +27,47 @@
   );
   $: voronoi = delaunay.voronoi([0, 0, $bounds.chartWidth, $bounds.chartHeight]);
 
-  // Manage interaction
-  const findLocation = (e) => {
-    const [x, y] = pointer(e, overlay);
-    const location = delaunay.find(x, y);
-    const god = $nodes[location];
-    interaction.highlight(getName(god));
-  };
-  const select = (e) => {
-    const [x, y] = pointer(e, overlay);
-    const location = delaunay.find(x, y);
-    const god = $nodes[location];
-    selection.highlight(god);
-  };
+  // Manage interaction on overlay rectangle
+  // const findLocation = (e) => {
+  //   const [x, y] = pointer(e, overlay);
+  //   const location = delaunay.find(x, y);
+  //   const god = $nodes[location];
+  //   interaction.highlight(getName(god));
+  // };
+  // const select = (e) => {
+  //   const [x, y] = pointer(e, overlay);
+  //   const location = delaunay.find(x, y);
+  //   const god = $nodes[location];
+  //   selection.highlight(god);
+  // };
+
+  $: selectionRelatedGods = [
+    ...new Set(
+      $currentLinks
+        .filter((link) => $selection && getName(link.source) === getName($selection))
+        .map((d) => d.target.name)
+    )
+  ];
 </script>
 
 <g data-name="overlay" transform={`translate(${$bounds.margins.left}, ${$bounds.margins.top})`}>
-  {#if debug}
-    {#each $nodes as p, i}
-      <path
-        d={voronoi.renderCell(i)}
-        fill="hotpink"
-        fill-opacity={0.2}
-        stroke="white"
-        stroke-opacity={1}
-      />
-    {/each}
-  {/if}
-  <rect
+  {#each $nodes as node, i}
+    <path
+      class:no-event={$selection && !selectionRelatedGods.includes(getName(node))}
+      d={voronoi.renderCell(i)}
+      fill="hotpink"
+      fill-opacity={debug ? 0.2 : 0}
+      stroke={debug ? "white" : "none"}
+      stroke-opacity={1}
+      on:click={() => selection.highlight(node)}
+      on:mousemove={() => interaction.highlight(getName(node))}
+      on:mouseenter={() => interaction.highlight(getName(node))}
+      on:focus={() => interaction.highlight(getName(node))}
+      on:mouseout={() => interaction.highlight(undefined)}
+      on:blur={() => interaction.highlight(undefined)}
+    />
+  {/each}
+  <!-- <rect
     style="pointer-events: {noPointerEvents ? 'none' : 'auto'}"
     bind:this={overlay}
     fill-opacity={0}
@@ -57,10 +79,16 @@
     on:focus={findLocation}
     on:mouseout={() => interaction.highlight(undefined)}
     on:blur={() => interaction.highlight(undefined)}
-  />
+  /> -->
 </g>
 
 <style>
+  path {
+    cursor: pointer;
+  }
+  path.no-event {
+    pointer-events: none;
+  }
   rect {
     /* cursor: url("/aztec-gods/assets/design-elements/pokeball.png") 12 12, auto; */
     cursor: pointer;
