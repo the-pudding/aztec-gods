c5523f430b92b73ef8963813938525f97bfc4eaf
lucguillemot <luc.guillemot@gmail.com>
2022-02-17 14:44:57 +0100

    update github pages

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -45,27 +45,3 @@
 <Pantheon />
 
 <Sources />
-
-<style>
-  h2 {
-    margin: 2rem 0 0.5rem 0;
-    font-size: 3rem;
-    line-height: 1.2;
-    text-transform: uppercase;
-    text-align: center;
-  }
-
-  /* @media only screen and (min-width: 30em) {
-    h2 {
-      margin: 3rem 0 1rem 0;
-      font-size: 2.5rem;
-    }
-  }
-
-  @media only screen and (min-width: 50em) {
-    h2 {
-      margin: 1.5rem 0 1rem 0;
-      font-size: 2rem;
-    }
-  } */
-</style>
