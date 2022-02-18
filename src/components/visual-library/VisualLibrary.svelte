1f7e89016f79cf323bd99ed9850a482dc57f32fa
Luc Guillemot <luc.guillemot@gmail.com>
2022-02-18 18:46:05 +0100

    Visual library (#34)
    * feat: add head example
    
    * content: update doc

---
+++ src/components/visual-library/VisualLibrary.svelte
@@ -0,0 +1,12 @@
+<script>
+  import doc from "$data/doc.json";
+  import WindHead from "$svg/optimized/wind-god-annotated.svg";
+  import Section from "$components/layout/Section.svelte";
+</script>
+
+<Section>
+  <div>{@html WindHead}</div>
+</Section>
+
+<style>
+</style>
