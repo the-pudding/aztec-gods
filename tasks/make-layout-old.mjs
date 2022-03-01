dcfad41674bb373e058a4f21422c5d52e227b9cd
lucguillemot <luc.guillemot@gmail.com>
2022-03-01 09:50:22 +0100

    refactor: simplify layout script to use only "aspect"

--- tasks/make-layout-old.mjs
+++ tasks/make-layout-old.mjs
@@ -8,10 +8,6 @@ const PADDING = 5;
 const GR = 1.62;
 
 let BASE = 20;
-
-// Domains
-const TYPE_SCALE = ["primordial", "creation", "elemental", "human", "secondary"];
-
 const radiusScale = scaleOrdinal()
   .domain(TYPE_SCALE)
   .range([BASE * (GR * 4), BASE * (GR * 3), BASE * (GR * 2), BASE * GR, BASE]);
@@ -23,7 +19,41 @@ const gods = JSON.parse(
   await readFile(new URL(`${CWD}/src/data/gods/tidy/gods.json`, import.meta.url))
 );
 
+const getRelationType = (d) => d.relation;
 const getName = (d) => d.name;
+
+const getGeometricPositions = (god) => {
+  switch (god) {
+    case "Huitzilopochtli":
+      return { x: 0.5, y: 0.65 };
+    case "Mayahuel":
+      return { x: 0.5, y: 0.2 };
+    case "Mictlantecuhtli":
+      return { x: 0.35, y: 0.35 };
+    case "Mixcoatl":
+      return { x: 0.5, y: 0.8 };
+    case "Ometeotl":
+      return { x: 0.5, y: 0.5 };
+    case "Quetzalcoatl":
+      return { x: 0.35, y: 0.5 };
+    case "Tezcatlipoca":
+      return { x: 0.5, y: 0.35 };
+    case "Tlaloc":
+      return { x: 0.35, y: 0.65 };
+    case "Tlaltecuhtli":
+      return { x: 0.65, y: 0.35 };
+    case "Xipe Totec":
+      return { x: 0.65, y: 0.5 };
+    case "Xiuhtecuhtli":
+      return { x: 0.65, y: 0.65 };
+    case "Xochiquetzal":
+      return { x: 0.2, y: 0.5 };
+    case "Yacatecuhtli":
+      return { x: 0.8, y: 0.5 };
+    default:
+      return { x: 0.5, y: 0.5 };
+  }
+};
 
 const rectCollide = (padding) => {
   let nodes;
@@ -78,10 +108,11 @@ const rectCollide = (padding) => {
   return force;
 };
 
-const calculateForceLayout = async () => {
+const calculateForceLayout = async (relation) => {
   let nodes = [...gods]; // Not pure...
-  let links = [...relations.filter((link) => link.relation === "aspect")];
-
+  let links = relation
+    ? [...relations.filter((link) => getRelationType(link) === relation)]
+    : [...relations];
   const simulation = forceSimulation(nodes);
 
   simulation
@@ -90,7 +121,7 @@ const calculateForceLayout = async () => {
       "link",
       forceLink(links).id((d) => getName(d))
     )
-    .force("center", forceCenter())
+    // .force("center", forceCenter())
     .alpha(1);
 
   for (let i = 0; i < 200; i++) simulation.tick();
@@ -107,19 +138,34 @@ const calculateForceLayout = async () => {
     links: links.map((l) => ({
       source: { x: l.source.x, y: l.source.y, name: l.source.name },
       target: { x: l.target.x, y: l.target.y, name: l.target.name },
+      relation: l.relation,
       index: l.index
     }))
   };
 };
 
 const getLayoutCoordinates = async () => {
-  const aspect = await calculateForceLayout();
+  const geometric = [...gods].map((god) => getGeometricPositions(god.name));
+  const allLinks = await calculateForceLayout(undefined);
+  const equality = await calculateForceLayout("cooperation");
+  const authority = await calculateForceLayout("authority");
+  const aspect = await calculateForceLayout("aspect");
 
   const nodes = gods.map((god, i) => ({
-    ...aspect.coord[i],
+    geometric: geometric[i],
+    allLinks: allLinks.coord[i],
+    equality: equality.coord[i],
+    authority: authority.coord[i],
+    aspect: aspect.coord[i],
     ...god
   }));
-  const linksCoord = aspect.links;
+  const linksCoord = {
+    geometric: [], // No links for geometric layout
+    allLinks: allLinks.links,
+    equality: equality.links,
+    authority: authority.links,
+    aspect: aspect.links
+  };
   fs.writeFileSync(`${CWD}/src/data/gods/tidy/nodes.json`, JSON.stringify(nodes));
   fs.writeFileSync(`${CWD}/src/data/gods/tidy/links.json`, JSON.stringify(linksCoord));
 };
