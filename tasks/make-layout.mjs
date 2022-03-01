de32b8db78bb1bbdffc19fc7f949cf0aa60f795f
lucguillemot <luc.guillemot@gmail.com>
2022-03-01 12:44:53 +0100

    refactor: data processing pipeline all in js

--- tasks/make-layout.mjs
+++ tasks/make-layout.mjs
@@ -1,8 +1,10 @@
 const CWD = process.cwd();
 import fs from "fs";
 import { readFile } from "fs/promises";
+import aq, { op } from "arquero";
 
 import { forceLink, forceSimulation, forceCenter, quadtree, scaleOrdinal } from "d3";
+
 // Layout
 const PADDING = 5;
 const GR = 1.62;
@@ -15,13 +17,6 @@ const TYPE_SCALE = ["primordial", "creation", "elemental", "human", "secondary"]
 const radiusScale = scaleOrdinal()
   .domain(TYPE_SCALE)
   .range([BASE * (GR * 4), BASE * (GR * 3), BASE * (GR * 2), BASE * GR, BASE]);
-
-const relations = JSON.parse(
-  await readFile(new URL(`${CWD}/src/data/gods/tidy/relations.json`, import.meta.url))
-);
-const gods = JSON.parse(
-  await readFile(new URL(`${CWD}/src/data/gods/tidy/gods.json`, import.meta.url))
-);
 
 const getName = (d) => d.name;
 
@@ -78,9 +73,61 @@ const rectCollide = (padding) => {
   return force;
 };
 
+// const relations = JSON.parse(
+//   await readFile(new URL(`${CWD}/src/data/gods/tidy/relations.json`, import.meta.url))
+// );
+// const gods = JSON.parse(
+//   await readFile(new URL(`${CWD}/src/data/gods/tidy/gods.json`, import.meta.url))
+// );
+
 const calculateForceLayout = async () => {
-  let nodes = [...gods]; // Not pure...
-  let links = [...relations.filter((link) => link.relation === "aspect")];
+  const raw = await aq.loadCSV(`${CWD}/src/data/gods/raw/light-db.tsv`, { delimiter: "\t" });
+
+  // Gods (nodes)
+  const gods = raw
+    .select({
+      ID: "id",
+      Name: "name",
+      type: "importance",
+      Field: "field",
+      Text: "bio",
+      "Other names or spelling": "spellings",
+      "Illustration source": "source",
+      "Aspects (other gods that can change into or him/her or share similar domains)": "aspect"
+    })
+    .derive({ name: (d) => op.trim(d.name) })
+    .filter((d) => d.id !== "cihuateteo")
+    .derive({
+      fertility: (d) => (op.match(d.field, "fertility") ? 1 : 0),
+      subsistence: (d) => (op.match(d.field, "subsistence") ? 1 : 0),
+      trade: (d) => (op.match(d.field, "trade") ? 1 : 0),
+      pleasure: (d) => (op.match(d.field, "pleasure") ? 1 : 0),
+      destruction: (d) => (op.match(d.field, "destruction") ? 1 : 0),
+      death: (d) => (op.match(d.field, "death") ? 1 : 0),
+      war: (d) => (op.match(d.field, "war") ? 1 : 0),
+      magic: (d) => (op.match(d.field, "magic") ? 1 : 0),
+      craft: (d) => (op.match(d.field, "craft") ? 1 : 0)
+    });
+
+  const godNames = [...gods.array("name")];
+
+  // relations (links)
+  const relations = gods
+    .select({ name: "source" }, "aspect")
+    .derive({ aspect: (d) => op.trim(d.aspect) })
+    .derive({ target: (d) => op.split(d.aspect, ", ") })
+    .unroll("target")
+    .select("source", "target");
+  // .derive({ is_unique: (d) => op.includes(godNames, d.target) })
+
+  const unique_relations = [...relations.objects()].filter((d) => godNames.includes(d.target));
+
+  fs.writeFileSync(`${CWD}/src/data/gods/tidy/gods.json`, JSON.stringify(gods.objects()));
+  fs.writeFileSync(`${CWD}/src/data/gods/tidy/relations.json`, JSON.stringify(unique_relations));
+
+  // Layout
+  let nodes = [...gods.objects()];
+  let links = [...unique_relations];
 
   const simulation = forceSimulation(nodes);
 
@@ -93,7 +140,7 @@ const calculateForceLayout = async () => {
     .force("center", forceCenter())
     .alpha(1);
 
-  for (let i = 0; i < 200; i++) simulation.tick();
+  for (let i = 0; i < 300; i++) simulation.tick();
 
   const coord = [
     ...nodes.map((n) => ({
@@ -101,9 +148,12 @@ const calculateForceLayout = async () => {
       y: n.y
     }))
   ];
-
+  const god_nodes = [...gods.objects()].map((god, i) => ({
+    ...coord[i],
+    ...god
+  }));
   return {
-    coord,
+    god_nodes,
     links: links.map((l) => ({
       source: { x: l.source.x, y: l.source.y, name: l.source.name },
       target: { x: l.target.x, y: l.target.y, name: l.target.name },
@@ -115,11 +165,9 @@ const calculateForceLayout = async () => {
 const getLayoutCoordinates = async () => {
   const aspect = await calculateForceLayout();
 
-  const nodes = gods.map((god, i) => ({
-    ...aspect.coord[i],
-    ...god
-  }));
+  const nodes = aspect.god_nodes;
   const linksCoord = aspect.links;
+
   fs.writeFileSync(`${CWD}/src/data/gods/tidy/nodes.json`, JSON.stringify(nodes));
   fs.writeFileSync(`${CWD}/src/data/gods/tidy/links.json`, JSON.stringify(linksCoord));
 };
