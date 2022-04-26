cdbbd074a7e69fcbd3c866c79a669e8f0d52085e
lucguillemot <luc.guillemot@gmail.com>
2022-04-26 17:42:50 +0200

    fix: improve Heading

--- src/components/layout/Heading.svelte
+++ src/components/layout/Heading.svelte
@@ -3,8 +3,8 @@
 <style>
   h2 {
     margin: 2rem 0 2rem 0;
-    font-size: 3rem;
-    line-height: 1.2;
+    font-size: 2.5rem;
+    line-height: 1.1;
     text-transform: uppercase;
     text-align: center;
   }
@@ -14,22 +14,19 @@
     content: "";
     background: var(--color-highlight);
     height: 3px;
-    width: 200px;
+    width: 120px;
     margin: 1.5rem auto 2rem;
     border-radius: 2px;
   }
 
-  /* @media only screen and (min-width: 30em) {
+  @media only screen and (min-width: 30em) {
     h2 {
-      margin: 3rem 0 1rem 0;
-      font-size: 2.5rem;
+      font-size: 3rem;
+      line-height: 1.2;
+    }
+
+    h2::after {
+      width: 200px;
     }
   }
-
-  @media only screen and (min-width: 50em) {
-    h2 {
-      margin: 1.5rem 0 1rem 0;
-      font-size: 2rem;
-    }
-  } */
 </style>
