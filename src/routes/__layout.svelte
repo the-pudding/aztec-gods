af17038bae96137395305842eab03f1d7ecd162e
lucguillemot <luc.guillemot@gmail.com>
2022-04-27 08:58:29 +0200

    chore: clean up

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,17 +3,7 @@
   import Header from "$components/Header.svelte";
 </script>
 
-<div class="bordered">
-  <!-- <Header /> -->
-  <main id="content">
-    <slot />
-  </main>
-</div>
-
-<style>
-  .bordered {
-    position: relative;
-    /* margin: 6px; */
-    box-shadow: 0 0 0 15px #222;
-  }
-</style>
+<!-- <Header /> -->
+<main id="content">
+  <slot />
+</main>
