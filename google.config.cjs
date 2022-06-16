cc044073aeb65585dcaea2fc3cfc316a9101e335
russellgoldenberg <russellgoldenberg@gmail.com>
2022-06-16 10:37:40 -0400

    update gdoc link

--- google.config.cjs
+++ google.config.cjs
@@ -1,11 +1,11 @@
 module.exports = [
-  {
-    id: "1EO3itVMyNdY2xPcxKa-sFL2w5dg6j0RtazaWRPx0oAI",
-    filepath: "src/data/doc.json"
-  }
-  // {
-  // 	"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
-  // 	"gid": "0",
-  // 	"filepath": "src/data/sheet.csv"
-  // }
+	{
+		id: "14_6OCtdZZBPSxgkdz2YsBLAxZJhUeFQxc_Qm-gFY_Nc",
+		filepath: "src/data/doc.json"
+	}
+	// {
+	// 	"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
+	// 	"gid": "0",
+	// 	"filepath": "src/data/sheet.csv"
+	// }
 ];
